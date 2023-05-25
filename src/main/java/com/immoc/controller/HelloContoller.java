118f08cbd2e563666ca4eaef69d4b83dca7e4394
GOSkyYLQ <41933674+GOSkyYLQ@users.noreply.github.com>
2023-05-25 11:26:12 +0800

    Update HelloContoller.java

--- src/main/java/com/immoc/controller/HelloContoller.java
+++ src/main/java/com/immoc/controller/HelloContoller.java
@@ -16,7 +16,7 @@ public class HelloContoller {
 	
 	@RequestMapping("/hello")
 	public Object hello() {
-		return "Hello My First springboot~~";
+		return "Hello My springboot~~";
 	}
 	
 	@RequestMapping("/getResource")
